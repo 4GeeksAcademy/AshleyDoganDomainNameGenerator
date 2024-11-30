7165dcf32798cd3dbf11f611b7f20bd3a57e83f7
Ashley Dogan <a.a.rappette@gmail.com>
2024-11-30 15:38:33 +0000

    not working

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+import "./styles.css";
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // arrays with the words
+  let pronoun = ["the", "our", "his"];
+  let adj = ["great", "big", "amazing"];
+  let noun = ["jogger", "racoon", "dog", "merchant", "driver"];
+
+  // generating the domains
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
 };
